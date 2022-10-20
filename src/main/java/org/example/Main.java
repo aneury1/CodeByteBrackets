da8d92ca168c029e2b03a3309f091831dc91836d
aneury <aneury1@gmail.com>
2022-10-20 16:41:42 -0400

    Add code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,66 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Optional;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+
+    public String BracketMatcher(String str) {
+        Stack<Character> chars = new Stack<>();
+        Boolean ret = false;
+
+        Stream<Character> strStream = str.chars().mapToObj(c-> (char)c);
+
+        ///Boolean contains = strStream.allMatch(c-> (c=='['||c=='('||c=='{'||c==']'||c==')'||c=='}'));
+
+        var k= ((strStream.filter(c-> (c=='['||c=='('||c=='{'||c==']'||c==')'||c=='}')).map(String::valueOf).collect(Collectors.joining())));
+
+        String filteredText = k.toString();
+        //// System.out.println(filteredText);
+        if(filteredText.length()==0)
+            return "1";
+
+        for (var j : filteredText.toCharArray())
+        {
+            if(j=='('||j=='{'||j == '[')
+                chars.push(j);
+            else{
+                if(chars.isEmpty())
+                    return "0";
+                else
+                {
+                    char pop = chars.pop();
+                    if(j ==')' && pop != '(')return "0";
+                    else if(j =='}' && pop != '{')return "0";
+                    else if(j ==']' && pop != '[')return "0";
+                    else{
+                        //System.out.println("SKipped");
+                        continue;
+                    }
+                }
+            }
+        }
+        if(chars.isEmpty())
+        {
+            return "1";
+        }
+        else
+            return "0";
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Main main =new Main();
+         System.out.println("[] => "+main.BracketMatcher("[]") );
+         System.out.println("(] => "+main.BracketMatcher("(]"));
+         System.out.println("[({})] => "+main.BracketMatcher("[({})]"));
+         System.out.println("[({[}})] => "+main.BracketMatcher("[({[}})]"));
+         System.out.println("(hello (world)) => "+main.BracketMatcher("(hello (world))"));
+         System.out.println("((hello (world)) => "+main.BracketMatcher("((hello (world))"));
+         System.out.println("Hello world => "+main.BracketMatcher("((hello (world))"));
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,2 +1,31 @@
-package org.example;public class MainTest {
+package org.example;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+
+
+    Main main;
+    @Before
+    public void loadMainClass(){
+        main = new Main();
+    }
+
+
+    @Test
+    public void ZeroReturnedOnBadBrackets(){
+        Assert.assertEquals(main.BracketMatcher("(coder)(byte))"  ),"0");
+    }
+
+    @Test
+    public void OneReturnedOnOkBrackets(){
+        Assert.assertEquals(main.BracketMatcher("(c(oder)) b(yte)"  ),"1");
+    }
+    @Test
+    public void OneReturnedOnNonBrackets(){
+        Assert.assertEquals(main.BracketMatcher("coder byte"  ),"1");
+    }
 }
